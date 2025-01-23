2629fed4bb10ffa6deb9319acebb0e578b049758
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2025-01-23 21:44:13 +0100

    Ejercicio 7 - Completo

--- colecciones/Tema3_Colecciones/src/boletin1/Ejercicio7.java
+++ colecciones/Tema3_Colecciones/src/boletin1/Ejercicio7.java
@@ -26,9 +26,11 @@ public class Ejercicio7 {
 		// usuario.
 		Scanner sc = new Scanner(System.in);
 
+		// Comprobamos si la opción introducida es distinta de 0, si es así volvemos a
+		// ejecutar el menú.
 		while (opc != 0) {
-			
-			// Imprimimos el menú
+
+			// Imprimimos el menú y solicitamos una opción al usuario y lo leemos.
 			System.out.println("Seleccione 0 - 1 - 2 en el siguiente menú: ");
 			System.out.println("1. Insertar palabra");
 			System.out.println("2. Buscar palabra");
@@ -38,39 +40,73 @@ public class Ejercicio7 {
 			opc = sc.nextInt();
 			sc.nextLine();
 
+			// Con este switch controlamos todas las opciones posibles introducidas por el
+			// usuario.
 			switch (opc) {
+			// En el caso de introducir 1...
 			case 1 -> {
+				// Le pedimos al usuario que introduzca una palabra en español (la clave) y la
+				// leemos.
 				System.out.print("Introduce una palabra en español: ");
 				palabraEsp = sc.nextLine();
+				// Le pedimos al usuario que introduzca una palabra en ingles (el valor) y la
+				// leemos.
 				System.out.print("Introduce su traducción en ingles: ");
 				palabraIng = sc.nextLine();
 
+				// Hacemos uso del método put para añadir las palabras introducidas por el
+				// usuario en el mapa.
 				diccionario.put(palabraEsp, palabraIng);
-				
+
+				// Salto de linea
 				System.out.println();
 			}
+
+			// En el caso de introducir 1...
 			case 2 -> {
+
+				// Le pedimos al usuario que introduzca la palabra en español para hacer la
+				// busqueda en el mapa por esa palabra y devolver su traducción, el valor.
 				System.out.print("Introduce la palabra de la que quieres saber su traducción: ");
 				palabraEsp = sc.nextLine();
 
+				// Comprobamos si esa palabra existe en el mapa con el método containsKey, si
+				// existe devolvemos el valor de esa palabra, la traducción en ingles de la
+				// palabra introducida.
 				if (diccionario.containsKey(palabraEsp)) {
+					// A la variable palabraIng le asignamos el valor que nos devuelve el método get
+					// de la clave introducida.
 					palabraIng = diccionario.get(palabraEsp);
+					// Imprimimos el valor de la plalabra introducida (palabra en ingles).
 					System.out.println("Su traducción es: " + palabraIng);
+
+					// En el caso de que no se encuentre en el mapa, indicamos que la palabra
+					// introducida no existe.
 				} else {
 					System.out.println("La palabra introducida no se encuentra en el mapa");
 				}
-				
+
+				// Salto de linea
 				System.out.println();
 
 			}
-			default -> {
+
+			// En el caso de introducir 0...
+			case 0 -> {
+				// Imprimimos un mensaje de salida
 				System.out.println("Saliendo...");
+			}
+
+			// En el caso de introducir una opción no valida...
+			default -> {
+
+				// Mostramos un mensaje de que la opción introducida es erronea
+				System.out.println("Opción introducida incorrecta");
 			}
 			}
 		}
-		
-		
-		//Cierre de Scanner
+
+		// Cierre de Scanner
 		sc.close();
 	}
 
